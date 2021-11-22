fb62d9beb8f1bb950062f821f6e4675976293c39
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2021-11-21 22:18:02 -0500

    uncomenting code

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/* require_once "vendor/econea/nusoap/src/nusoap.php";
+require_once "vendor/econea/nusoap/src/nusoap.php";
 
 $namespace = "test";
 $server = new soap_server();
@@ -28,6 +28,4 @@ $POST_DATA = file_get_contents("php://input");
 print($POST_DATA);
 $server->service($POST_DATA);
 exit();
- */
 
- print("Hol 2a");
