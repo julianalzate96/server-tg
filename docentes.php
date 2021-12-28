4d3681f86fba6411d143341f54cf136b4c13444d
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2021-12-28 01:27:37 -0500

    new function implemented in docentes functions

--- docentes.php
+++ docentes.php
@@ -1,6 +1,7 @@
 <?php
 
 $conn = require "db.php";
+require 'utils.php';
 
 function swDocentes($cedula){
     global $conn;
@@ -11,6 +12,10 @@ function swDocentes($cedula){
 
     mysqli_free_result($result);
     return $row;
+}
+
+function swDocentesAsignatura($codigo_asignatura){
+    return buildResponseArray("SELECT D.cedula, D.nombre FROM DOCENTE D INNER JOIN ASIGNATURA A ON D.codigo_asignatura = A.codigo_asignatura WHERE D.codigo_asignatura = '$codigo_asignatura'");
 }
 
 
