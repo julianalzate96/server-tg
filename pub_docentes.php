f97e7b6ffa1cd55aba01013ea39bd5dc7af75f27
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2021-12-30 03:16:49 -0500

    cors bug fixed

--- pub_docentes.php
+++ pub_docentes.php
@@ -1,8 +1,7 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 require_once 'vendor/autoload.php';
 require_once "vendor/econea/nusoap/src/nusoap.php";
