a640001a4ec1bf1630b5dc5a1e5229f082d74274
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2022-01-02 23:40:14 -0500

    response updated for swDocentes

--- docentes.php
+++ docentes.php
@@ -1,17 +1,8 @@
 <?php
-
-$conn = require "db.php";
 require 'utils.php';
 
 function swDocentes($cedula){
-    global $conn;
-    
-    $result = $conn->query("SELECT cedula, nombre, nivel_academico, correo, tiempo_experiencia, tiempo_poli FROM DOCENTE WHERE cedula = $cedula");
-
-    $row = mysqli_fetch_assoc($result);
-
-    mysqli_free_result($result);
-    return $row;
+    return buildResponse("SELECT cedula, nombre, nivel_academico, correo, tiempo_experiencia, tiempo_poli FROM DOCENTE WHERE cedula = $cedula");
 }
 
 function swDocentesAsignatura($codigo_asignatura){
