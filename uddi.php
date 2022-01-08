ced2685355ae216ebf23e89d7fcccb8e6db5766c
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2022-01-08 02:20:27 -0500

    uddi created

---
+++ uddi.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'utils.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch($method) {
+    case 'GET': 
+        if($_GET["type"] == 'categories'){
+            $result = buildResponseArray('SELECT * FROM CATEGORIA');
+        }
+        if($_GET["type"] == 'services'){
+            $result = buildResponseArray('SELECT * FROM SERVICIO WHERE id_categoria='.$_GET["category"]);
+        }
+    break;
+}
+
+echo json_encode($result);
