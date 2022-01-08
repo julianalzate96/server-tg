ef3cf42ebb8bad468d441e93e6266e177f6ae91b
Julian Alzate <julian.alzate@parrolabs.com>
2022-01-08 02:42:23 -0500

    headers updated

--- uddi.php
+++ uddi.php
@@ -4,7 +4,7 @@ include 'utils.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 $method = $_SERVER['REQUEST_METHOD'];
 
