285479241f2bf499be5cc1cbe541444a17c2df7b
Julian Alzate <andres_alzate82142@elpoli.edu.co>
2022-01-22 00:00:46 -0500

    auth fixed

--- auth.php
+++ auth.php
@@ -8,11 +8,13 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 $token = getenv("TOKEN");
 
 if(isset($_POST["email"])){
-    $domain = "@poli.edu.co";
+    $domain = "@elpoli.edu.co";
 
     if(strpos($_POST["email"], $domain) !== false){
         echo json_encode($token);
     } else{
         echo json_encode(false);
     }
+}else{
+    echo json_encode(false);
 }
